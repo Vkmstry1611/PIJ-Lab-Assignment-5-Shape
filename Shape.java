0234f898a9efa3fd1d85a64f36d67f6648b8b9fd
unknown <vidish162005@gmail.com>
2025-03-07 10:20:36 +0530

    added Shape class

--- Shape.java
+++ Shape.java
@@ -1,3 +1,29 @@
 abstract class Shape{
     
+    protected double dim_one;
+    protected double dim_two;
+    protected double dim_three;
+    protected int num_sides;
+    protected String shapeName;
+
+    // Parameterized constructor
+    public Shape(String shapeName, double dim_one, double dim_two, double dim_three, int num_sides) {
+        this.shapeName = shapeName;
+        this.dim_one = dim_one;
+        this.dim_two = dim_two;
+        this.dim_three = dim_three;
+        this.num_sides = num_sides;
+    }
+
+    // Abstract methods
+    abstract public double calculateArea();
+    abstract public double calculatePerimeter();
+
+    // Getter method for number of sides
+    public int getNumSides() {
+        return num_sides;
+    }
+
+
+
 }
