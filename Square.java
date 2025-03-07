37e3aa675f827cc07ef5115607cd628b111ea0d1
unknown <vidish162005@gmail.com>
2025-03-07 10:22:50 +0530

    added Square.java

---
+++ Square.java
@@ -0,0 +1,13 @@
+class Square extends Shape {
+    public Square(double side) {
+        super("Square", side, 0, 0, 4);
+    }
+
+    public double calculateArea() {
+        return dim_one * dim_one;
+    }
+
+    public double calculatePerimeter() {
+        return 4 * dim_one;
+    }
+}
