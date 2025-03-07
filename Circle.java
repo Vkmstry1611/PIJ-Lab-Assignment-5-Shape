75dd85cca93381f7e79d912128cf6ddeb754cacf
unknown <vidish162005@gmail.com>
2025-03-07 10:41:49 +0530

    added Circle.java

---
+++ Circle.java
@@ -0,0 +1,13 @@
+class Circle extends Shape {
+    public Circle(double radius) {
+        super("Circle", radius, 0, 0, 0);
+    }
+
+    public double calculateArea() {
+        return Math.PI * dim_one * dim_one;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * Math.PI * dim_one;
+    }
+}
