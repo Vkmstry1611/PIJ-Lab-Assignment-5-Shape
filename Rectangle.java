8359e821cdf08fd7bdab996811d88c787ac62c8f
unknown <vidish162005@gmail.com>
2025-03-07 10:21:37 +0530

    added Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,13 @@
+class Rectangle extends Shape {
+    public Rectangle(double length, double breadth) {
+        super("Rectangle", length, breadth, 0, 4);
+    }
+
+    public double calculateArea() {
+        return dim_one * dim_two;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * (dim_one + dim_two);
+    }
+}
